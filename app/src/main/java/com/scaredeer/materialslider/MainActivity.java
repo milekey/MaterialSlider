8b8ff9f1f6303dfcd48cd286293e008630f171c1
milekey <hata@mihr.net>
2020-12-21 23:28:10 +0900

    Add buttons.

--- app/src/main/java/com/scaredeer/materialslider/MainActivity.java
+++ app/src/main/java/com/scaredeer/materialslider/MainActivity.java
@@ -1,14 +1,14 @@
 package com.scaredeer.materialslider;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.slider.Slider;
 
-public class MainActivity extends AppCompatActivity implements Slider.OnChangeListener {
+public class MainActivity extends AppCompatActivity {
     private TextView textView;
     private static final int MAX_INDEX = 99;
 
@@ -17,15 +17,17 @@ public class MainActivity extends AppCompatActivity implements Slider.OnChangeLi
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity);
 
+        textView = findViewById(R.id.textView);
+
         Slider slider = findViewById(R.id.slider);
-        slider.addOnChangeListener(this);
+        slider.addOnChangeListener((slider1, value, fromUser) -> textView.setText(String.format("%.2f", value)));
         slider.setLabelFormatter(value -> String.valueOf(Math.round(MAX_INDEX * value)));
 
-        textView = findViewById(R.id.textView);
-    }
-
-    @Override
-    public void onValueChange(@NonNull Slider slider, float value, boolean fromUser) {
-        textView.setText(String.valueOf(value));
+        Button buttonCenter = findViewById(R.id.buttonCenter);
+        buttonCenter.setOnClickListener(view -> slider.setValue(0.5f));
+        Button buttonLeft = findViewById(R.id.buttonLeft);
+        buttonLeft.setOnClickListener(view -> slider.setValue(Math.max(0, slider.getValue() - 0.1f)));
+        Button buttonRight = findViewById(R.id.buttonRight);
+        buttonRight.setOnClickListener(view -> slider.setValue(Math.min(slider.getValue() + 0.1f, 1.0f)));
     }
 }
