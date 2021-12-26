ef7b5f85c5c204c1deb94d5468901fa4a79404c0
milekey <hata@mihr.net>
2021-12-26 16:07:13 +0900

    Minor dependency updates.

--- app/src/main/java/com/scaredeer/materialslider/MainActivity.java
+++ app/src/main/java/com/scaredeer/materialslider/MainActivity.java
@@ -1,5 +1,6 @@
 package com.scaredeer.materialslider;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.TextView;
@@ -13,6 +14,7 @@ public class MainActivity extends AppCompatActivity {
     private TextView textView;
     private static final int MAX_INDEX = 100;
 
+    @SuppressLint("DefaultLocale")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
